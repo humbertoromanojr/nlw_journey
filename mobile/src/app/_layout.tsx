98b7ec9adfb73f5bb059d2c9964d8f900e731f31
humbertojr <astronomi@gmail.com>
2025-01-14 17:15:36 -0300

    Import and Use loading component

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -10,25 +10,27 @@ import {
   Inter_500Medium,
 } from "@expo-google-fonts/inter";
 
+import { Loading } from "@/components/loading";
+
 export default function Layout() {
-    const [fontsLoaded] = useFonts({
-        Inter_400Regular,
-        Inter_600SemiBold,
-        Inter_500Medium,
-    });
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_500Medium,
+  });
 
-    if (!fontsLoaded) {
-        return
-    }
+  if (!fontsLoaded) {
+    return <Loading />
+  }
 
-    return (
-        <View className="flex-1 bg-zinc-950">
-        <StatusBar
-            barStyle="light-content"
-            backgroundColor="transparent"
-            translucent
-        />
-        <Slot />
-        </View>
-    );
+  return (
+    <View className="flex-1 bg-zinc-950">
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Slot />
+    </View>
+  );
 }
